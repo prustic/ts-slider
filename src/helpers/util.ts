f2d751416ed0d47078df1aa779f948caa91ce88b
Petar Rustic <rustic.pr@pg.com>
2021-03-08 20:43:34 +0100

    no infinitize

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,4 +1,4 @@
-import { ReactNode, RefObject } from 'react'
+import { RefObject } from 'react'
 
 export function getPositionX(
     event:
@@ -14,62 +14,4 @@ export function getElementDimensions(ref: RefObject<HTMLDivElement>) {
     const width = ref.current ? ref.current.clientWidth : 0
     const height = ref.current ? ref.current.clientHeight : 0
     return { width, height }
-}
-
-export const infinitize = (input: ReactNode[]) => {
-    if (!Array.isArray(input)) {
-        throw new TypeError('Expected an array')
-    }
-
-    return new Proxy(input, {
-        get(target: ReactNode[], name: PropertyKey, receiver: ReactNode): ReactNode {
-            if (typeof name !== 'string') {
-                return Reflect.get(target, name, receiver)
-            }
-
-            let index = Number(name)
-
-            if (Number.isNaN(index)) {
-                return Reflect.get(target, name, receiver)
-            }
-
-            if (index % target.length === 0) {
-                index = 0
-            } else if (index < 0) {
-                index = target.length + (index % target.length)
-            } else if (index >= target.length) {
-                index %= target.length
-            }
-
-            return target[index]
-        },
-        set(
-            target: ReactNode[],
-            name: PropertyKey,
-            value: ReactNode,
-            receiver: ReactNode
-        ): boolean {
-            if (typeof name !== 'string') {
-                return Reflect.set(target, name, value, receiver)
-            }
-
-            const index = Number(name)
-
-            if (Number.isNaN(index)) {
-                return Reflect.set(target, name, value, receiver)
-            }
-
-            if (index < 0) {
-                if (index % target.length === 0) {
-                    target[0] = value
-                } else {
-                    target[target.length + (index % target.length)] = value
-                }
-            } else {
-                target[index] = value
-            }
-
-            return true
-        },
-    })
 }
